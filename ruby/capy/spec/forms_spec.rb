af3c224af6dab5d2a7a4599c35a372bde033284a
Cibele <cibelermallmann@gmail.com>
2022-07-04 23:53:03 -0300

    Validando notificação

--- ruby/capy/spec/forms_spec.rb
+++ ruby/capy/spec/forms_spec.rb
@@ -1,8 +1,8 @@
 
-## No console, pode usar os comandos abaixo para testar se os elementos são encontrados
-## $('#flash'); -> flash é o id > # pq é um id > verifica se o elemento existe
-## $('#flash').text(); -> flash é o id > # pq é um id > text é a função
-## fill_in usa o atributo name ou id
+# No console, pode usar os comandos abaixo para testar se os elementos são encontrados
+# $('#flash'); -> flash é o id > # pq é um id > verifica se o elemento existe
+# $('#flash').text(); -> flash é o id > # pq é um id > text é a função
+# fill_in usa o atributo name ou id
 
 describe 'Forms' do
     it 'login com sucesso' do
@@ -16,6 +16,13 @@ describe 'Forms' do
 
         expect(find('#flash').visible?).to be true
 
-    end
+        # Ambos código abaixo validam a notificação
+        # Validar texto identico -> eql
+        # Validar se o texto contém no elemento -> include
+        # 1- Obtendo o texto e perguntando se o valor contém no texto (comparação de string)
+        # 2 - Se existe o conteúdo (string) no elemento (verifica direto no elemento)
 
+        expect(find('#flash').text).to include 'Olá, Tony Stark. Você acessou a área logada!'
+        expect(find('#flash')). to have_content 'Olá, Tony Stark. Você acessou a área logada!'
+    end
 end
